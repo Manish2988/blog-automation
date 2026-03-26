9a601aaba18e9f913faf97031819b85803a2b9f7
Manish2988 <59115723+Manish2988@users.noreply.github.com>
2026-03-26 09:00:27 +0530

    Log presence of REFRESH_TOKEN in environment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_SECRET,
   "https://developers.google.com/oauthplayground"
 );
-
+console.log("REFRESH_TOKEN:", process.env.REFRESH_TOKEN ? "Present" : "Missing");
 oauth2Client.setCredentials({
   refresh_token: process.env.REFRESH_TOKENS
 });
